5e28fb1f63c260f8a9224857560a856717c4e7ed
secureanytime <jameswoodinfo@gmail.com>
2026-07-27 17:22:31 +0530

    Initialize conf.py with project settings

---
+++ conf.py
@@ -0,0 +1,9 @@
+project = 'Secure_Anytime'
+copyright = '2026'
+author = 'Admin'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'alabaster' # Screenshot wala classic white theme
