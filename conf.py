95e23c48a98739338f0dc3960ee1a77f834c897b
secureanytime <jameswoodinfo@gmail.com>
2026-07-28 18:55:30 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -7,3 +7,4 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster' # Screenshot wala classic white theme
+<meta name="google-site-verification" content="Yel_C69xoq0MiKn3qV4oh4aahOYq5GevwyXt6ETpHaU" />
